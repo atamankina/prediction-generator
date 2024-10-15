516c1827d4eebd05f9147e222fdcf7b7dc1b4157
Galina Atamankina <g.atamankina@gmail.com>
2024-10-15 10:35:55 +0200

    Adding the lambda function module.

---
+++ terraform/modules/lambda/variables.tf
@@ -0,0 +1,29 @@
+variable "function_name" {
+    description = "The name of the Lambda function."
+    type = string
+}
+
+variable "lambda_role_arn" {
+    description = "The ARN of the Lambda execution role"
+    type = string
+}
+
+variable "handler" {
+    description = "The function handler in the code"
+    type = string
+}
+
+variable "runtime" {
+    description = "The runtime for the Lmabda function"
+    type = string
+}
+
+variable "lambda_zip_path" {
+    description = "The path to the Lambda ZIP file"
+    type = string
+}
+
+variable "lambda_environment_vars" {
+    description = "Environment variables for the Lambda function"
+    type = map(string)
+}
